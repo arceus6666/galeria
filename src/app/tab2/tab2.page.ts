a77da501dfd41bc8079d2172f4edcba9a579b6a6
Daniel Mendoza <arceus6666@hotmail.com>
2019-01-03 22:31:13 -0400

    m

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  currentImage: any;
+
+  constructor(private photoService:PhotoService) {  }
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+
+}
